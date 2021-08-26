54899b0059a03ca4911bc02d95aca8db830023bd
Pavlo <pbeloshein@gmail.com>
2021-08-26 23:16:32 +0300

    add logger

---
+++ avaj_launcher/Logger.java
@@ -0,0 +1,15 @@
+package avaj_launcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logger {
+    private static final List<String> simulationLog = new ArrayList<>();
+    public static void log(String s) {
+        simulationLog.add(s);
+    }
+
+    public static List<String> getSimulationLog() {
+        return simulationLog;
+    }
+}
